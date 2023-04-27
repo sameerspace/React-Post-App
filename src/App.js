2d951b46c97aface1c5d6fa1738e092948d5bbca
sameerspace <sameer.sam2k17@gmail.com>
2023-04-27 14:30:04 +0500

    Moved Conditional Route rendering from component to route

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ import CreatePostPage from './pages/CreatePost';
 import EditPostPage from './pages/EditPostPage';
 import { usePosts } from './contexts/PostContext';
 import { useEffect } from 'react';
+import { useAuth } from './contexts/AuthContext';
 
 
 
 const App = () => {
   const { fetchAndSetPosts } = usePosts()
+  const { userIsLoggedIn } = useAuth()
+  const user = userIsLoggedIn()
 
   useEffect(() => fetchAndSetPosts)
-
+  
   return (
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
+      <Route path='/' element={ user ?  <HomePage/> : <SignInPage/>}/>
       <Route path='/posts' element={<HomePage/>}/>
       <Route path='/login' element={<SignInPage/>} />
       <Route path='/register' element={<RegisterPage/>} />
-      <Route path='/my-posts' element={<MyPostsPage/>} />
-      <Route path='/create-post' element={<CreatePostPage/>} />
-      <Route path='/edit-post' element={<EditPostPage/>} />
+      <Route path='/my-posts' element={user ? <MyPostsPage/> : <SignInPage/>} />
+      <Route path='/create-post' element={ user ? <CreatePostPage/> : <SignInPage/> } />
+      <Route path='/edit-post' element={ user ? <EditPostPage/> : <SignInPage/>} />
     </Routes>
   );
 }
